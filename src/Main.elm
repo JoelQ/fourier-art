6205bebb3b4ce90a41519e5575117b66567b9ae4
Joël Quenneville <joelq@thoughtbot.com>
2019-07-05 12:18:43 +0200

    Set up elm architecture

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,41 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 
 
-main : Html a
+main : Program Flags Model Msg
 main =
+    Browser.sandbox
+        { init = Polar 1 45
+        , view = view
+        , update = update
+        }
+
+
+type alias Flags =
+    ()
+
+
+type alias Model =
+    Polar
+
+
+type alias Polar =
+    { magnitude : Int
+    , theta : Int
+    }
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
     text "hello"
